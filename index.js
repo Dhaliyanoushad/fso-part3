d93d49cc508e73001296911ceef76fc8c81b5821
Daliya <daliyadeutsch@gmail.com>
2025-02-22 14:52:59 +0530

    ex 3.16

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const Contact = require("./models/contacts");
 
 const PORT = process.env.PORT || 3001;
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(express.static("dist")); //The application can now be used from the backend address http://localhost:3001
 app.use(cors());
 app.use(express.json());
@@ -15,38 +29,6 @@ morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-
-// This is the token that we defined in the previous part
-// We want to log the body of POST requests
-// morgan.token("body", (req) =>
-//   req.method === "POST" && req.body ? req.body.name : ""
-// );
-// app.use(
-//   morgan(":method :url :status :res[content-length] - :response-time ms :body")
-// );
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hello",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Lucifer",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Sun",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/api/persons", (request, response) => {
   // response.json(persons);
@@ -67,10 +49,6 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  // const id = Number(request.params.id);
-  // persons = persons.filter((person) => person.id !== id);
-
-  // response.status(204).end();
   Contact.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
@@ -81,29 +59,6 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.post("/api/persons", (request, response) => {
-  // const body = request.body;
-
-  // if (!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: "name or number is missing",
-  //   });
-  // }
-
-  // if (persons.find((person) => person.name === body.name)) {
-  //   return response.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
-
-  // const person = {
-  //   id: persons.length + 1,
-  //   name: body.name,
-  //   number: body.number,
-  // };
-
-  // persons = persons.concat(person);
-  // response.json(person);
-
   const body = request.body;
 
   if (body.name === undefined) {
@@ -131,3 +86,9 @@ app.get("/info", (request, response) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
