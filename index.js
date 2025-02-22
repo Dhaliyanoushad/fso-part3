146a8f821f1aa9b554485c6f4c7947df4a0e1c6d
Daliya <daliyadeutsch@gmail.com>
2025-02-22 14:27:47 +0530

    ex 3.15

--- index.js
+++ index.js
@@ -67,10 +67,17 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+  // const id = Number(request.params.id);
+  // persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+  // response.status(204).end();
+  Contact.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
 app.post("/api/persons", (request, response) => {
